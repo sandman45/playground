7b1b5b42cc88b290654d36aec227ba7a6ad06eba
sandman45 <msanders45@gmail.com>
2019-04-12 16:47:36 -0600

    updated stuff

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ app.options('/*', function(req, res){
   res.header('Access-Control-Allow-Origin', req.headers.origin || "*");
   res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,HEAD,DELETE,OPTIONS');
   res.header('Access-Control-Allow-Headers', 'content-Type,x-requested-with');
-  res.send(200);
+  res.status(200).send(200);
 });
 
 //-- require all routes in playground directory to run security check
