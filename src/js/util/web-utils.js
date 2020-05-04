f57286e750ba370d6689ac38e0b30e011c0af018
sandman45 <msanders45@gmail.com>
2020-05-04 16:54:51 -0600

    adding flyby for tie fighters.. added controls

--- src/js/util/web-utils.js
+++ src/js/util/web-utils.js
@@ -9,11 +9,13 @@ function checkCookie(){
 
 function showCookieFail(){
     // do something here
-    alert('Show Cookie Failed');
+    // alert('Show Cookie Failed');
+    console.log('Show Cookie Failed');
 }
 
 
 // within a window load,dom ready or something like that place your:
 const cookie = checkCookie();
 
-alert(`Cookies Enabled: ${cookie}`);
+// alert(`Cookies Enabled: ${cookie}`);
+console.log(`Cookies Enabled: ${cookie}`);
